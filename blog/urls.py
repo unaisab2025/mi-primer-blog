934353141a1f20a308caf1c650e2238aca9f84d7
unai2025 <patriciosabanew@gmail.com>
2026-02-06 12:42:15 -0300

    Mi aplicación proyecto Django, primer commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.lista_public, name=' lista_public'),
+path('publicacion/nueva/', views.publicacion_nueva, name='publicacion_nueva'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,6 @@ def lista_public(request):
     publicaciones = Publicacion.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
 
     return render(request, 'blog/lista_public.html', {'publicaciones':publicaciones})
-    
+def publicacion_nueva(request):
+    # Aquí irá el código para manejar el formulario más adelante
+    return render(request, 'blog/publicacion_editar.html', {})
